f81b21309e02a7658405ab669f85fc012d88a179
FernandoJHR <fernando.hermosilla.j.r@gmail.com>
2025-06-17 20:37:55 -0400

    añadido en la tarde

--- Hr-crud/src/components/Form.js
+++ Hr-crud/src/components/Form.js
@@ -1,30 +1,36 @@
-import react, {useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 
-function Form({addOrUpdateItem, itemToEdit}) {const[inputValue,setinputValue] = useState(");
+function Form({addOrUpdateItem, itemToEdit}) {
+const[inputValue,setinputValue] = useState('');
 
-useEffect(()=> {
-if (itemToEdit){
-    setinputValue(itemToEdit.Value);
-} else {
-    setinputValue(");
-}
-},[itemToEdit]);
+    useEffect(()=> {
+    if (itemToEdit){
+        setinputValue(itemToEdit.Value);
+    } else {
+        setinputValue('');
+    }
+    },[itemToEdit]);
 const handleSubmit = (e) =>{
     e.preventDefault();
     if (inputValue.trim()){
         addOrUpdateItem(inputValue);
-        setinputValue(");
+        setinputValue('');
     }
 };
 
 return(
-    <form onSubmit={handleSubmit}>
-        <input
-        type="text"
-        value={inputValue}
-        onChange={(e)=>
-    setinputValue(e.target.value)}
-    />
-    <button
-    </form>
-)
+<form onSubmit={handleSubmit}>
+    <input
+    type="text"
+    value={inputValue}
+    onChange={(e)=>
+setinputValue(e.target.value)}
+/>
+<button type="submit">{itemToEdit?
+'Actualizar' :'agregar'}</button>
+</form>
+);
+}
+export default Form;
+
+//Este Form sirve para crear y utilizar elementos//
---
+++ Hr-crud/src/components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Item({item, deleteItem, editItem}) {
+    return(
+        <li>
+            {item.value}
+            <button onClick={() => editItem(item)}>Editar</button>
+            <button onClick={() => deleteItem(item.id)}>Eliminar</button>
+        </li>
+    );
+}
+
+export default Item;
+
+//Este representa un solo item de la lista//
